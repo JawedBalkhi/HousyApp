93b620ba7664d65c5ec4941b7c38424036ff9c65
Jawed-balkhi <student3@hydrologic.com>
2020-06-03 16:32:16 +0200

    api met data

--- src/components/typewonning.js
+++ src/components/typewonning.js
@@ -1,46 +1,78 @@
-import React,{Component} from 'react';
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import axios from 'axios';
+
+const Dankmemes = () => {
+  const [chartData, setChartData] = useState({});
 
 
-class Typewonning extends Component{
-    constructor(props){
-        super(props);
-            this.state ={
-                items :[],
-                isLoded: false,
-            
+  const chart = () => {
+    let empSal = [];
+    let empAge = [];
+    axios
+      .get("http://dummy.restapiexample.com/api/v1/employees")
+      .then(res => {
+        console.log(res);
+        for (const dataObj of res.data.data) {
+          empSal.push(parseInt(dataObj.employee_salary));
+          empAge.push(parseInt(dataObj.employee_age));
         }
-    }
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res=>res.json())
-        .then(json => {
-        this.setState({
-            isLoded: true,
-            items :json,
-        })
+        setChartData({
+          labels: empAge,
+          datasets: [
+            {
+              label: "level of thiccness",
+              data: empSal,
+              backgroundColor: ["rgba(75, 192, 192, 0.6)"],
+              borderWidth: 4
+            }
+          ]
         });
-    }
-    render(){
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log(empSal, empAge);
+  };
 
-        var{isLoded , items} =this.state;
-        if (!isLoded){
-            return <dic>Loading....</dic>
-        }
+  useEffect(() => {
+    chart();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Dankmemes</h1>
+      <div>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { text: "THICCNESS SCALE", display: true },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                    beginAtZero: true
+                  },
+                  gridLines: {
+                    display: false
+                  }
+                }
+              ],
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false
+                  }
+                }
+              ]
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
-        else {
-        return(
-            <div>
-                    <ul>
-                        {items.map(item=> (
-                            <li key={item.id===1}>
-                               Name : {item.address.geo.lng} | Email :{item.address.geo.lat}
-                            </li>
-
-                        ))};
-                    </ul>
-            </div>
-        );
-    }
-}
-}
-export default Typewonning;
+export default Dankmemes;
