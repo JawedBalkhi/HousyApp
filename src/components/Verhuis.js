38950b86d13eeb1d39bdbc7b02d5e7b8daa937fd
Jawed-balkhi <student3@hydrologic.com>
2020-06-04 22:59:09 +0200

    testtt

--- src/components/Verhuis.js
+++ src/components/Verhuis.js
@@ -1,10 +1,10 @@
 import React,{Component} from 'react';
 import {Grid,Cell} from 'react-mdl';
-import Select from 'react-select';
-import { DropDownList } from '@progress/kendo-react-dropdowns';
-import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+import axios from 'axios';
+import data from '../data.json'
+
 
 class Verhuis extends Component {
    constructor(props){
@@ -12,25 +12,62 @@ class Verhuis extends Component {
        this.state={
         items:[],
         value: "",
-        isLoaded: false
+        isLoaded: true,
+     
        }
+       this.onCitySelect = this.onCitySelect.bind(this)
+
+   }
+
+   componentWillMount() {
+    axios.get('../public/users.json') // JSON File Path
+      .then( response => {
+        this.setState({
+        userList: response.data
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
    }
   
       
-        componentDidMount() {
-         fetch( "https://jsonplaceholder.typicode.com/users")
-            .then(res => res.json())
-            .then(json =>{
-                this.setState({
-                    isLoaded:true,
-                    items:json,
-                })
-            });
+        // componentDidMount() {
+        //  fetch( "https://jsonplaceholder.typicode.com/users")
+        //     .then(res => res.json())
+        //     .then(json =>{
+        //         this.setState({
+        //             isLoaded:true,
+        //             items:json,
+        //         })
+        //     });
           
+        // }
+        componentDidMount() {
+            fetch("src/data.json")
+            .then(function(data) {
+              console.log(data);
+              
+            });
+        }
+        onCitySelect(e){
+
+          // // fetch( "https://jsonplaceholder.typicode.com/users" + value)
+          // .then(res => res.json())
+          // .then(json =>{
+          //     this.setState({
+          //         isLoaded:true,
+          //         items:json,
+          //     })
+          // });
+        
+
         }
    
     render() {
+
      const {isLoaded,items} = this.state;
+  
      
      const name = items.map(item => (  item.name ))
   
@@ -42,10 +79,15 @@ class Verhuis extends Component {
             <Grid className="verhuis-Grid">
             <Cell col ={8}>
             <div>
-            <Dropdown options={name} onChange={e => this.setState({ value: e.value })}  setState={this.value}  placeholder="Select an option" />;                
-        
+            <Dropdown options={name} onChange={e => { this.onCitySelect(e); this.setState({ value: e.value})}} setState={this.value} placeholder="Select an option" />;                
+            
             {this.state.value}
             </div>
+            <div>
+               
+         
+         </div>
+        
                  </Cell>
                  </Grid>
          )
